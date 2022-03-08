83e9e0d599332dffc9f95d884f7daf1d49cfbb5f
Melodi <62381002+Melodi17@users.noreply.github.com>
2022-03-08 15:21:16 +1000

    Update SDK.cs

--- ream/SDK/SDK.cs
+++ ream/SDK/SDK.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ream.SDK
+﻿namespace Ream.SDK
 {
     public class ExternalVariableAttribute : Attribute
     {
